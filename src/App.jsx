f4b5e79d7f8a49dc80cca070a2046fcad9272583
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-02-09 07:59:57 +0000

    Change project configuration to use create react app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Signup from "./pages/signup/signup";
 import Dashboard from "./pages/dashboard/dashboard";
 import ExpenseAdd from "./pages/expense-add/expense-add";
 import ExpenseView from "./pages/expense-view/expense-view";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ExpenseProvider from "./services/expense.service";
 
 function App() {
   return (
@@ -13,8 +14,24 @@ function App() {
         <Routes>
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
-          <Route exact path="/expense-add" element={<ExpenseAdd />} />
+          <Route
+            exact
+            path="/dashboard"
+            element={
+              <ExpenseProvider>
+                <Dashboard />
+              </ExpenseProvider>
+            }
+          />
+          <Route
+            exact
+            path="/expense-add"
+            element={
+              <ExpenseProvider>
+                <ExpenseAdd />
+              </ExpenseProvider>
+            }
+          />
           <Route exact path="/expense-view" element={<ExpenseView />} />
         </Routes>
       </div>
